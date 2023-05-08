83a3fc10bbca51d6ff88e0f52fc9eaaf0a21d063
Joe Marie Aquino <aquino.092130@tarlac.sti.edu.ph>
2023-05-08 10:05:24 +0800

    stud reg

--- StudentEvaluationApp/frmStudentReg.Designer.cs
+++ StudentEvaluationApp/frmStudentReg.Designer.cs
@@ -30,16 +30,42 @@
         {
             tableLayoutPanel1 = new TableLayoutPanel();
             label1 = new Label();
+            maskedTextBox1 = new MaskedTextBox();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            label5 = new Label();
+            comboBox1 = new ComboBox();
             tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
-            tableLayoutPanel1.BackColor = Color.Blue;
-            tableLayoutPanel1.ColumnCount = 2;
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.BackColor = Color.White;
+            tableLayoutPanel1.ColumnCount = 10;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
+            tableLayoutPanel1.Controls.Add(label5, 8, 0);
+            tableLayoutPanel1.Controls.Add(textBox3, 7, 0);
+            tableLayoutPanel1.Controls.Add(label4, 6, 0);
+            tableLayoutPanel1.Controls.Add(textBox2, 5, 0);
+            tableLayoutPanel1.Controls.Add(label3, 4, 0);
+            tableLayoutPanel1.Controls.Add(label2, 2, 0);
             tableLayoutPanel1.Controls.Add(label1, 0, 0);
+            tableLayoutPanel1.Controls.Add(maskedTextBox1, 1, 0);
+            tableLayoutPanel1.Controls.Add(textBox1, 3, 0);
+            tableLayoutPanel1.Controls.Add(comboBox1, 9, 0);
             tableLayoutPanel1.Dock = DockStyle.Fill;
             tableLayoutPanel1.Location = new Point(0, 0);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -57,6 +83,79 @@
             label1.Size = new Size(70, 15);
             label1.TabIndex = 0;
             label1.Text = "Student No.";
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(83, 3);
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(74, 23);
+            maskedTextBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(163, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(64, 15);
+            label2.TabIndex = 2;
+            label2.Text = "First Name";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(243, 3);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(74, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(323, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(63, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Last Name";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(403, 3);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(74, 23);
+            textBox2.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(483, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(47, 30);
+            label4.TabIndex = 6;
+            label4.Text = "Middle Name";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(563, 3);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(74, 23);
+            textBox3.TabIndex = 7;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(643, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(37, 15);
+            label5.TabIndex = 8;
+            label5.Text = "Suffix";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Jr.", "Sr." });
+            comboBox1.Location = new Point(723, 3);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(74, 23);
+            comboBox1.TabIndex = 9;
             // 
             // frmStudentReg
             // 
@@ -80,5 +179,14 @@
 
         private TableLayoutPanel tableLayoutPanel1;
         private Label label1;
+        private TextBox textBox2;
+        private Label label3;
+        private Label label2;
+        private MaskedTextBox maskedTextBox1;
+        private TextBox textBox1;
+        private Label label5;
+        private TextBox textBox3;
+        private Label label4;
+        private ComboBox comboBox1;
     }
 }

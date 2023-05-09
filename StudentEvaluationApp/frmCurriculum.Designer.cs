0827059cd05ad00aabcb90d8175a69c12746bdc8
Joe Marie Aquino <aquino.092130@tarlac.sti.edu.ph>
2023-05-09 16:37:13 +0800

    curi

--- StudentEvaluationApp/frmCurriculum.Designer.cs
+++ StudentEvaluationApp/frmCurriculum.Designer.cs
@@ -33,11 +33,14 @@
             label1 = new Label();
             bSave = new Button();
             dgvCurricuVer = new DataGridView();
+            lbGotID = new Label();
+            button1 = new Button();
+            textBox1 = new TextBox();
             colID = new DataGridViewTextBoxColumn();
             colCurricuDes = new DataGridViewTextBoxColumn();
+            colManage = new DataGridViewImageColumn();
             colEdit = new DataGridViewImageColumn();
             colDel = new DataGridViewImageColumn();
-            lbGotID = new Label();
             tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dgvCurricuVer).BeginInit();
             SuspendLayout();
@@ -45,18 +48,21 @@
             // tableLayoutPanel1
             // 
             tableLayoutPanel1.ColumnCount = 3;
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.33333F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333359F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.3333359F));
             tableLayoutPanel1.Controls.Add(tbCurricuVer, 0, 1);
             tableLayoutPanel1.Controls.Add(label1, 0, 0);
             tableLayoutPanel1.Controls.Add(bSave, 2, 1);
-            tableLayoutPanel1.Controls.Add(dgvCurricuVer, 0, 2);
+            tableLayoutPanel1.Controls.Add(dgvCurricuVer, 0, 3);
             tableLayoutPanel1.Controls.Add(lbGotID, 2, 0);
+            tableLayoutPanel1.Controls.Add(button1, 1, 2);
+            tableLayoutPanel1.Controls.Add(textBox1, 0, 2);
             tableLayoutPanel1.Dock = DockStyle.Fill;
             tableLayoutPanel1.Location = new Point(0, 0);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
-            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.RowCount = 4;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
             tableLayoutPanel1.RowStyles.Add(new RowStyle());
             tableLayoutPanel1.RowStyles.Add(new RowStyle());
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
@@ -82,10 +88,13 @@
             // 
             // bSave
             // 
-            bSave.BackColor = Color.White;
+            bSave.AutoSize = true;
+            bSave.BackColor = Color.ForestGreen;
+            bSave.FlatStyle = FlatStyle.Flat;
+            bSave.ForeColor = Color.White;
             bSave.Location = new Point(535, 18);
             bSave.Name = "bSave";
-            bSave.Size = new Size(75, 23);
+            bSave.Size = new Size(75, 27);
             bSave.TabIndex = 2;
             bSave.Text = "Save";
             bSave.UseVisualStyleBackColor = false;
@@ -98,16 +107,41 @@
             dgvCurricuVer.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dgvCurricuVer.BackgroundColor = Color.White;
             dgvCurricuVer.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvCurricuVer.Columns.AddRange(new DataGridViewColumn[] { colID, colCurricuDes, colEdit, colDel });
+            dgvCurricuVer.Columns.AddRange(new DataGridViewColumn[] { colID, colCurricuDes, colManage, colEdit, colDel });
             tableLayoutPanel1.SetColumnSpan(dgvCurricuVer, 3);
             dgvCurricuVer.Dock = DockStyle.Fill;
-            dgvCurricuVer.Location = new Point(3, 47);
+            dgvCurricuVer.Location = new Point(3, 80);
             dgvCurricuVer.Name = "dgvCurricuVer";
             dgvCurricuVer.ReadOnly = true;
             dgvCurricuVer.RowTemplate.Height = 25;
-            dgvCurricuVer.Size = new Size(794, 400);
+            dgvCurricuVer.Size = new Size(794, 367);
             dgvCurricuVer.TabIndex = 3;
             dgvCurricuVer.CellContentClick += dgvCurricuVer_CellContentClick;
+            // 
+            // lbGotID
+            // 
+            lbGotID.AutoSize = true;
+            lbGotID.Location = new Point(535, 0);
+            lbGotID.Name = "lbGotID";
+            lbGotID.Size = new Size(47, 15);
+            lbGotID.TabIndex = 4;
+            lbGotID.Text = "hidden ";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(269, 51);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 5;
+            button1.Text = "Search";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(3, 51);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 6;
             // 
             // colID
             // 
@@ -122,6 +156,12 @@
             colCurricuDes.HeaderText = "Curriculum Version";
             colCurricuDes.Name = "colCurricuDes";
             colCurricuDes.ReadOnly = true;
+            // 
+            // colManage
+            // 
+            colManage.HeaderText = "";
+            colManage.Name = "colManage";
+            colManage.ReadOnly = true;
             // 
             // colEdit
             // 
@@ -142,15 +182,6 @@
             colDel.Name = "colDel";
             colDel.ReadOnly = true;
             colDel.Width = 5;
-            // 
-            // lbGotID
-            // 
-            lbGotID.AutoSize = true;
-            lbGotID.Location = new Point(535, 0);
-            lbGotID.Name = "lbGotID";
-            lbGotID.Size = new Size(47, 15);
-            lbGotID.TabIndex = 4;
-            lbGotID.Text = "hidden ";
             // 
             // frmCurriculum
             // 
@@ -178,10 +209,13 @@
         private Label label1;
         private Button bSave;
         private DataGridView dgvCurricuVer;
+        private Label lbGotID;
+        private Button button1;
+        private TextBox textBox1;
         private DataGridViewTextBoxColumn colID;
         private DataGridViewTextBoxColumn colCurricuDes;
+        private DataGridViewImageColumn colManage;
         private DataGridViewImageColumn colEdit;
         private DataGridViewImageColumn colDel;
-        private Label lbGotID;
     }
 }

7b627f9194de9de10b29194ff4e32b283cfb9258
Joe Marie Aquino <aquino.092130@tarlac.sti.edu.ph>
2023-05-24 09:12:20 +0800

    curriculum select course

--- StudentEvaluationApp/frmManageCurriculum.cs
+++ StudentEvaluationApp/frmManageCurriculum.cs
@@ -32,17 +32,43 @@ namespace StudentEvaluationApp
         {
             if (dgvCourses.Columns[e.ColumnIndex].Name.Equals("colToRight"))
             {
-                if (dgvToCurriculum.Rows.Count == 0) 
+                if (dgvToCurriculum.Rows.Count == 0)
                 {
                     dgvToCurriculum.Rows.Add(
                         dgvCourses.Rows[e.RowIndex].Cells["colCourseID"].Value,
                         dgvCourses.Rows[e.RowIndex].Cells["colCourseCode"].Value,
                         dgvCourses.Rows[e.RowIndex].Cells["colCourse"].Value,
-                        dgvCourses.Rows[e.RowIndex].Cells["colUnits"].Value
-                    );
+                        dgvCourses.Rows[e.RowIndex].Cells["colUnits"].Value);
+                }
+                else
+                {
+                    int matchCount = 0;
+                    foreach (DataGridViewRow r in dgvToCurriculum.Rows)
+                    {
+                        if (r.Cells["colIDToCv"].Value.ToString().Equals
+                            (dgvCourses.Rows[e.RowIndex].Cells["colCourseID"].Value.ToString()))
+                        {
+                            matchCount++;
+                        }
+                    }
+
+                    if (matchCount != 0)
+                    {
+                        MessageBox.Show("The Course is already added.");
+                    }
+                    else
+                    {
+                        dgvToCurriculum.Rows.Add(
+                            dgvCourses.Rows[e.RowIndex].Cells["colCourseID"].Value,
+                            dgvCourses.Rows[e.RowIndex].Cells["colCourseCode"].Value,
+                            dgvCourses.Rows[e.RowIndex].Cells["colCourse"].Value,
+                            dgvCourses.Rows[e.RowIndex].Cells["colUnits"].Value);
+                    }
+
                 }
 
-                
+
+
             }
         }
     }

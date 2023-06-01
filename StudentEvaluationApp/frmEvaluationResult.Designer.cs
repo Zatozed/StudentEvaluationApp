528991a22a2c05866629e3bf7c2b1cf37d7c62b2
Joe Marie Aquino <aquino.092130@tarlac.sti.edu.ph>
2023-06-01 16:48:47 +0800

    responsive dgv

--- StudentEvaluationApp/frmEvaluationResult.Designer.cs
+++ StudentEvaluationApp/frmEvaluationResult.Designer.cs
@@ -30,7 +30,6 @@
         {
             tableLayoutPanel1 = new TableLayoutPanel();
             button2 = new Button();
-            button1 = new Button();
             label1 = new Label();
             label2 = new Label();
             lbStudName = new Label();
@@ -44,8 +43,11 @@
             lbYear = new Label();
             label12 = new Label();
             lbSem = new Label();
-            tableLayoutPanel2 = new TableLayoutPanel();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            dataGridView1 = new DataGridView();
             tableLayoutPanel1.SuspendLayout();
+            flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -56,8 +58,7 @@
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 24F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 19F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 19F));
-            tableLayoutPanel1.Controls.Add(button2, 4, 7);
-            tableLayoutPanel1.Controls.Add(button1, 3, 7);
+            tableLayoutPanel1.Controls.Add(button2, 4, 5);
             tableLayoutPanel1.Controls.Add(label1, 2, 0);
             tableLayoutPanel1.Controls.Add(label2, 0, 1);
             tableLayoutPanel1.Controls.Add(lbStudName, 1, 1);
@@ -71,21 +72,21 @@
             tableLayoutPanel1.Controls.Add(lbYear, 4, 2);
             tableLayoutPanel1.Controls.Add(label12, 3, 3);
             tableLayoutPanel1.Controls.Add(lbSem, 4, 3);
-            tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 0, 4);
+            tableLayoutPanel1.Controls.Add(flowLayoutPanel1, 0, 4);
             tableLayoutPanel1.Dock = DockStyle.Fill;
             tableLayoutPanel1.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point);
             tableLayoutPanel1.Location = new Point(0, 0);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
-            tableLayoutPanel1.RowCount = 8;
+            tableLayoutPanel1.RowCount = 6;
             tableLayoutPanel1.RowStyles.Add(new RowStyle());
             tableLayoutPanel1.RowStyles.Add(new RowStyle());
             tableLayoutPanel1.RowStyles.Add(new RowStyle());
             tableLayoutPanel1.RowStyles.Add(new RowStyle());
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle());
-            tableLayoutPanel1.Size = new Size(800, 450);
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.Size = new Size(799, 451);
             tableLayoutPanel1.TabIndex = 0;
             // 
             // button2
@@ -95,32 +96,18 @@
             button2.BackColor = Color.ForestGreen;
             button2.FlatStyle = FlatStyle.Flat;
             button2.ForeColor = Color.White;
-            button2.Location = new Point(696, 420);
+            button2.Location = new Point(695, 421);
             button2.Name = "button2";
             button2.Size = new Size(101, 27);
             button2.TabIndex = 4;
             button2.Text = "Print and Save";
             button2.UseVisualStyleBackColor = false;
             // 
-            // button1
-            // 
-            button1.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            button1.AutoSize = true;
-            button1.BackColor = Color.ForestGreen;
-            button1.FlatStyle = FlatStyle.Flat;
-            button1.ForeColor = Color.White;
-            button1.Location = new Point(570, 420);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 27);
-            button1.TabIndex = 3;
-            button1.Text = "Save";
-            button1.UseVisualStyleBackColor = false;
-            // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(307, 0);
+            label1.Location = new Point(305, 0);
             label1.Margin = new Padding(3, 0, 3, 10);
             label1.Name = "label1";
             label1.Size = new Size(181, 15);
@@ -142,7 +129,7 @@
             // 
             lbStudName.AutoSize = true;
             lbStudName.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            lbStudName.Location = new Point(155, 25);
+            lbStudName.Location = new Point(154, 25);
             lbStudName.Margin = new Padding(3, 0, 3, 10);
             lbStudName.Name = "lbStudName";
             lbStudName.Size = new Size(41, 15);
@@ -164,7 +151,7 @@
             // 
             lbStudNum.AutoSize = true;
             lbStudNum.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            lbStudNum.Location = new Point(155, 50);
+            lbStudNum.Location = new Point(154, 50);
             lbStudNum.Margin = new Padding(3, 0, 3, 10);
             lbStudNum.Name = "lbStudNum";
             lbStudNum.Size = new Size(41, 15);
@@ -186,7 +173,7 @@
             // 
             lbProgram.AutoSize = true;
             lbProgram.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            lbProgram.Location = new Point(155, 75);
+            lbProgram.Location = new Point(154, 75);
             lbProgram.Margin = new Padding(3, 0, 3, 10);
             lbProgram.Name = "lbProgram";
             lbProgram.Size = new Size(41, 15);
@@ -197,7 +184,7 @@
             // 
             label8.AutoSize = true;
             label8.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label8.Location = new Point(499, 25);
+            label8.Location = new Point(496, 25);
             label8.Margin = new Padding(3, 0, 3, 10);
             label8.Name = "label8";
             label8.Size = new Size(133, 15);
@@ -208,7 +195,7 @@
             // 
             lbCv.AutoSize = true;
             lbCv.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            lbCv.Location = new Point(651, 25);
+            lbCv.Location = new Point(647, 25);
             lbCv.Margin = new Padding(3, 0, 3, 10);
             lbCv.Name = "lbCv";
             lbCv.Size = new Size(41, 15);
@@ -219,7 +206,7 @@
             // 
             label10.AutoSize = true;
             label10.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label10.Location = new Point(499, 50);
+            label10.Location = new Point(496, 50);
             label10.Margin = new Padding(3, 0, 3, 10);
             label10.Name = "label10";
             label10.Size = new Size(40, 15);
@@ -230,7 +217,7 @@
             // 
             lbYear.AutoSize = true;
             lbYear.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            lbYear.Location = new Point(651, 50);
+            lbYear.Location = new Point(647, 50);
             lbYear.Margin = new Padding(3, 0, 3, 10);
             lbYear.Name = "lbYear";
             lbYear.Size = new Size(48, 15);
@@ -241,7 +228,7 @@
             // 
             label12.AutoSize = true;
             label12.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label12.Location = new Point(499, 75);
+            label12.Location = new Point(496, 75);
             label12.Margin = new Padding(3, 0, 3, 10);
             label12.Name = "label12";
             label12.Size = new Size(72, 15);
@@ -252,46 +239,58 @@
             // 
             lbSem.AutoSize = true;
             lbSem.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point);
-            lbSem.Location = new Point(651, 75);
+            lbSem.Location = new Point(647, 75);
             lbSem.Margin = new Padding(3, 0, 3, 10);
             lbSem.Name = "lbSem";
             lbSem.Size = new Size(48, 15);
             lbSem.TabIndex = 17;
             lbSem.Text = "label13";
             // 
-            // tableLayoutPanel2
+            // flowLayoutPanel1
             // 
-            tableLayoutPanel2.ColumnCount = 1;
-            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
-            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel2.Location = new Point(3, 103);
-            tableLayoutPanel2.Name = "tableLayoutPanel2";
-            tableLayoutPanel2.RowCount = 2;
-            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            tableLayoutPanel2.Size = new Size(146, 99);
-            tableLayoutPanel2.TabIndex = 18;
+            flowLayoutPanel1.AutoScroll = true;
+            flowLayoutPanel1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            flowLayoutPanel1.BackColor = Color.DarkSeaGreen;
+            tableLayoutPanel1.SetColumnSpan(flowLayoutPanel1, 5);
+            flowLayoutPanel1.Controls.Add(dataGridView1);
+            flowLayoutPanel1.Dock = DockStyle.Fill;
+            flowLayoutPanel1.Location = new Point(3, 103);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(793, 312);
+            flowLayoutPanel1.TabIndex = 18;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(3, 3);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(787, 309);
+            dataGridView1.TabIndex = 0;
             // 
             // frmEvaluationResult
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(799, 451);
             Controls.Add(tableLayoutPanel1);
+            MinimumSize = new Size(815, 490);
             Name = "frmEvaluationResult";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "frmEvaluationResult";
             Load += frmEvaluationResult_Load;
+            SizeChanged += frmEvaluationResult_SizeChanged;
             tableLayoutPanel1.ResumeLayout(false);
             tableLayoutPanel1.PerformLayout();
+            flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private TableLayoutPanel tableLayoutPanel1;
-        private Button button1;
         private Button button2;
         private Label label1;
         private Label label2;
@@ -306,6 +305,7 @@
         private Label lbYear;
         private Label label12;
         private Label lbSem;
-        private TableLayoutPanel tableLayoutPanel2;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private DataGridView dataGridView1;
     }
 }
--- StudentEvaluationApp/frmEvaluationResult.cs
+++ StudentEvaluationApp/frmEvaluationResult.cs
@@ -17,13 +17,23 @@ namespace StudentEvaluationApp
         {
             InitializeComponent();
 
-            lbStudName.Text = dbh.GetFullName(Properties.Settings.Default.CurrentStudentID);
-            lbStudNum.Text = dbh.GetStudNum(Properties.Settings.Default.CurrentStudentID);
+            //lbStudName.Text = dbh.GetFullName(Properties.Settings.Default.CurrentStudentID);
+            //lbStudNum.Text = dbh.GetStudNum(Properties.Settings.Default.CurrentStudentID);
+            lbProgram.Text = Properties.Settings.Default.CurrentProgram;
+
+            lbCv.Text = Properties.Settings.Default.CurrentCv;
+            lbYear.Text = Properties.Settings.Default.CurrentYear.ToString();
+            lbSem.Text = Properties.Settings.Default.CurrentSem.ToString();
         }
 
         private void frmEvaluationResult_Load(object sender, EventArgs e)
         {
             //if sem = sem1 do sem 2
+        }
+
+        private void frmEvaluationResult_SizeChanged(object sender, EventArgs e)
+        {
+            dataGridView1.Size = new Size((int)(this.Width * 0.98),(int)(this.Height * 0.6306));
         }
     }
 }
